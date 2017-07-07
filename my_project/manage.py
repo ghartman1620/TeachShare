e37f716030f38536af1b1c6ad128e71187aeec34
Yang Wu <ywu140@ucsc.edu>
2017-07-06 18:26:33 -0700

    working myproject

--- my_project/manage.py
+++ my_project/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TeachShare_WebApp.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
