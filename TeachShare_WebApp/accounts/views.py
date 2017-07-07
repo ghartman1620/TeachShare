0afdea0d048f29f3705bec91f4768e7e73520fdb
NevilleMorgan <namorgan@ucsc.edu>
2017-07-06 19:17:58 -0700

    TeachShare_WebApp working login

--- TeachShare_WebApp/accounts/views.py
+++ TeachShare_WebApp/accounts/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-	name = 'teachshare'
+	name = 'TeachShare'
 	numbers = [1,2,3,4,5,6]
 	args = {'myName': name, 'numbers': numbers }
 	return render(request,'accounts/home.html',args)
