683613e28828162174f0b68f3309a94359b646e6
Gabriel Hartman <ghartman@ucsc.edu>
2017-07-08 14:42:25 -0700

    different way of views.py and urls.py in account

--- TeachShare_WebApp/accounts/urls.py
+++ TeachShare_WebApp/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.views import login
 urlpatterns = [
 	
-url(r'^$',views.home),
-url(r'^login/$',login, {'template_name': 'accounts/login.html'})
-url(r'^signup/$',signup, {'template_name': 'accounts/signup.html'})
+	url(r'^$',views.home),
+	url(r'^login/$', views.login, name='login'),
+	url(r'^signup/$', views.signup, name='signup'),
 ]
--- TeachShare_WebApp/accounts/views.py
+++ TeachShare_WebApp/accounts/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,9 @@ def home(request):
 	numbers = [1,2,3,4,5,6]
 	args = {'myName': name, 'numbers': numbers }
 	return render(request,'accounts/home.html',args)
+	
+def login(request):
+	return render(request, 'accounts/login.html',None)
+
+def signup(request):
+	return render(request, 'accounts/login.html',None)
