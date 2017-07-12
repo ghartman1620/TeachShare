59308b22ba91a5bd303b1528b779a67ac9373d16
Gabriel Hartman <ghartman@ucsc.edu>
2017-07-12 16:15:07 -0700

    registering. views.py, urls.py, signup.html

--- TeachShare_WebApp/accounts/urls.py
+++ TeachShare_WebApp/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login
+
+app_name = 'account'
 urlpatterns = [
 	
-	url(r'^$',views.home),
+	url(r'^$',views.home, name='home'),
 	url(r'^login/$', views.login, name='login'),
 	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^register/$', views.register, name='register'),
 ]
--- TeachShare_WebApp/accounts/views.py
+++ TeachShare_WebApp/accounts/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -16,3 +18,19 @@ def login(request):
 
 def signup(request):
 	return render(request, 'accounts/signup.html',None)
+
+def register(request):
+	try:
+		pw = request.POST['pw']
+		pwConfirm = request.POST['pwConfirm']
+		username = request.POST['username']
+		email = request.POST['email']
+		
+	except(KeyError):
+	   return HttpResponse("One or more fields is empty.")
+	else:
+		if(pw != pwConfirm):
+			return HttpResponse("Password and confirm password do not match.")
+		user = User.objects.create_user(username, email, pw)
+		return HttpResponseRedirect(reverse('account:home'))
+		
