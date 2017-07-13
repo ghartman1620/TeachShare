a8669bdbc78a1c9d39a700ab0a78033336a72f42
Yang Wu <ywu140@ucsc.edu>
2017-07-13 15:17:57 -0700

    userprofile signal connects to admin page

--- TeachShare_WebApp/accounts/models.py
+++ TeachShare_WebApp/accounts/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 
@@ -12,3 +14,11 @@ class UserProfile(models.Model):
 	phone = models.IntegerField( default=0)
 	city = models.CharField(max_length=100, default='')
 
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+
+
+post_save.connect(create_profile, sender=User)
