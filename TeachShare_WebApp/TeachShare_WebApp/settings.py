26d36bf240c6a3072fd38277f3d6be404982c352
Akul Goyal <akulagoyal@gmail.com>
2017-07-21 09:19:40 -0700

    initial upload

--- TeachShare_WebApp/TeachShare_WebApp/settings.py
+++ TeachShare_WebApp/TeachShare_WebApp/settings.py
@@ -115,9 +115,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
 LOGIN_REDIRECT_URL = '/account/'
 LOGIN_URL = '/account/login'
