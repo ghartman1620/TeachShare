b56559e41c61e70794c4af3ad3ba661fe74f7c86
bryan <bmccoid@gmail.com>
2018-01-25 13:00:26 -0800

    added fetch to Test component

--- Frontend/teach-share/src/components/Test.vue
+++ Frontend/teach-share/src/components/Test.vue
@@ -1,22 +1,25 @@
 <template>
-  <h1>{{req}}</h1>
+  <div>
+    <h1>{{ message }}</h1>
+    <button v-on:click="loadPost">Load Post</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Test',
-  data: {
-    req: '',
-  },
-  mounted: function(){
-    this.req = fetch('http://localhost:8000/api/posts/1/')
-    .then(function(resp){
-      return resp.json();
-    }).catch(err => console.error('Error', err))
+  data: function() {
+    return {
+      message: 'This is a message.'
+    }
   },
   methods: {
     loadPost(){
-
+      console.log('beginning fetch');
+      fetch('http://localhost:8000/api/posts/1/')
+      .then(resp => resp.json())
+      .then(resp => this.message = resp)
+      .catch(err => console.error('Error', err))
     }
   }
 }
