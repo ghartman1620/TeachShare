158b7923496bfe9bd565e332e8e7fa34fcfbeac1
bryan <bmccoid@gmail.com>
2018-01-25 11:29:46 -0800

    added vuejs project in the Frontend directory (to go with the capitalized naming convention

---
+++ Frontend/teach-share/src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Yay you made a vueJS component...'
+    }
+  }
+}
+</script>
+
