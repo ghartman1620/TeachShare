859bb87904d324ee01df2fb2822aa6a1d5fd6f68
Bryan M <bmccoid@ucsc.edu>
2018-02-03 19:35:52 -0800

    Added Base.vue to start framing out a basic page

--- TeachShare_WebApp/TeachShare_WebApp/settings.py
+++ TeachShare_WebApp/TeachShare_WebApp/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
