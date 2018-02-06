310dde06c9c515d676d997892d2da78ebad35384
bryan <bmccoid@gmail.com>
2018-02-06 12:18:00 -0800

    added a test as an example.

--- TeachShare_WebApp/posts/tests.py
+++ TeachShare_WebApp/posts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from posts.models import Post
+from accounts.models import User
 
-# Create your tests here.
+
+class PostCreateTestCase(TestCase):
+    def setUp(self):
+        print("PostCreate [setUp]")
+        # nothing yet
+
+    def test_can_create(self):
+        # factory = APIRequestFactory()
+        # request = factory.get('/posts/1/')
+        # print(request)
+
+        User.objects.create_user('bryan', 'bmccoid@ucsc.edu')
+        u = User.objects.first()
+        # Post.objects.create(title='test post', content={}, likes=0, user=u)
+        # p = Post.objects.first()
+        # print(Post.objects.all())
+
+        client = APIClient()
+        resp = client.post('/api/posts/', {
+            'title': 'test post',
+            'content': {},
+            'likes': 0,
+            'user': u.pk,
+        }, format='json')
+        # resp = client.get('/api/posts/1/')
+        print(dir(resp))
+        print(resp.content)
+        print(resp.data)
+        print(resp.items)
+        self.assertEqual(resp.status_code, 201)
