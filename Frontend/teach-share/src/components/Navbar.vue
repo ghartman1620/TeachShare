f7b2229c4bf2bbf106600e766f5aea9f3039c2a7
unknown <ghartman@ucsc.edu>
2018-02-13 12:26:00 -0800

    merging navbar to jeff's

--- Frontend/teach-share/src/components/Navbar.vue
+++ Frontend/teach-share/src/components/Navbar.vue
@@ -1,41 +1,42 @@
 <template>
-  <nav class="navbar fixed-top navbar-inverse navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar fixed-top navbar-inverse navbar-expand-lg navbar-dark dark-green">
+      <div class="container">
+      <!-- brand section -->
+      <router-link class="navbar-brand" to="/"><img class="img-responsive ts-logo" src="/static/tslogo.png"></router-link>
 
-    <!-- brand section -->
-    <router-link class="navbar-brand" to="/">TeachShare</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
 
-        <!-- home link -->
-        <li class="nav-item active">
-          <router-link class="nav-link" to="/base">Home <span class="sr-only">(current)</span></router-link>
-        </li>
 
-        <!-- create a post link -->
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name: 'create'}">Create Post</router-link>
-        </li>
+          <!-- create a post link -->
+          <li class="nav-item">
+            <router-link class="nav-link" to="/create">Create Post</router-link>
+          </li>
 
-        <!-- just an example dropdown -->
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown example
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
+          <!-- just an example dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="Account Profile" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              Profile
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Your post feed</a>
+              <a class="dropdown-item" href="#">Your posts</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Account details</a>
+            </div>
+          </li>
+        </ul>
 
-      <!-- search box component -->
-      <search-box></search-box>
+        <!-- search box component -->
+        <div class="search-container">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="button"> <!--class="btn btn-outline-basic"--><img class="img search-button" src="/static/search.svg"></button>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
@@ -45,7 +46,7 @@ import Vue from 'vue';
 import SearchBox from './SearchBox.vue';
 
 export default Vue.component('nav-bar', {
-    components: { SearchBox },
+    components: SearchBox,
     props: ['query'],
     data() {
       return {
@@ -63,9 +64,21 @@ export default Vue.component('nav-bar', {
   text-decoration: underline;
   text-decoration-style: solid;
   text-decoration-color: #46E;
+
 }
 .nav {
   height: 100px;
+}
+.dark-green {
+  background-color: #3B896A
+}
+.ts-logo {
+  height: 45px;
+  width: auto;
+}
+.search-button {
+  height: 20px;
+  width: auto;
 }
 
 </style>
