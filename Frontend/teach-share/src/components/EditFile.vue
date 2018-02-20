76b3b90cc9dc040c0c83056ae952ad1bcdde744a
Brian Tran <thebbht@gmail.com>
2018-02-20 12:25:31 -0800

    File editor in progress

---
+++ Frontend/teach-share/src/components/EditFile.vue
@@ -0,0 +1,30 @@
+<template>
+	<body>
+	<file-upload :fileLimit="20" title="Upload Files">
+	</file-upload>
+	<router-link :to="{name: 'create'}">
+        <button @click="submit" type="submit" :disabled="!allFilesUploadComplete" class="btn btn-primary btn-block">
+            <span v-if="!allFilesUploadComplete">Please Select File(s) to upload</span>
+            <span v-else>Submit Image(s)</span>
+        </button>
+        </router-link>
+       </body>
+</template>
+
+<script>
+import Vue from "vue";
+import FileUpload from "./FileUpload";
+export default Vue.component("edit-file", {
+	components: {FileUpload},
+	computed: {
+		allFilesUploadComplete(){
+			return this.$store.getters.allFilesUploadComplete;
+		}
+	}
+});
+</script>
+
+
+<style>
+
+</style>
--- Frontend/teach-share/src/components/FileComponent.vue
+++ Frontend/teach-share/src/components/FileComponent.vue
@@ -9,64 +9,6 @@
 						<h5 class="card-title text-center">
 							<a href="#" class="card-link">Title</a>
 						</h5>
-						<p class="card-text text-center">Description</p>
-					</div>
-					<div class = "col-sm"
-						<button type="button" class="btn btn-primary btn-sm">Remove</button>
-					</div>
-				</div>
-				</div>
-			</div>
-		</div>
-		</li>
-		<li>
-		<div class = "container">
-			<div class = "row">			
-				<div class="card" style="width: 18rem;">
-				<div class="card-body">
-					<div class = "col-md"
-						<h5 class="card-title text-center">
-							<a href="#" class="card-link">Title</a>
-						</h5>
-						<p class="card-text text-center">Description</p>
-					</div>
-					<div class = "col-sm"
-						<button type="button" class="btn btn-primary btn-sm">Remove</button>
-					</div>
-				</div>
-				</div>
-			</div>
-		</div>
-		</li>
-		<li>
-		<div class = "container">
-			<div class = "row">			
-				<div class="card" style="width: 18rem;">
-				<div class="card-body">
-					<div class = "col-md"
-						<h5 class="card-title text-center">
-							<a href="#" class="card-link">Title</a>
-						</h5>
-						<p class="card-text text-center">Description</p>
-					</div>
-					<div class = "col-sm"
-						<button type="button" class="btn btn-primary btn-sm">Remove</button>
-					</div>
-				</div>
-				</div>
-			</div>
-		</div>
-		</li>
-		<li>
-		<div class = "container">
-			<div class = "row">			
-				<div class="card" style="width: 18rem;">
-				<div class="card-body">
-					<div class = "col-md"
-						<h5 class="card-title text-center">
-							<a href="#" class="card-link">Title</a>
-						</h5>
-						<p class="card-text text-center">Description</p>
 					</div>
 					<div class = "col-sm"
 						<button type="button" class="btn btn-primary btn-sm">Remove</button>
