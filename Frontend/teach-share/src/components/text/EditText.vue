ae907c8b803ffaec56a2f10d73ee25a045da9e7c
unknown <ghartman@ucsc.edu>
2018-03-01 11:25:37 -0800

    edit text in text folder; edit text has some padding; merge;

--- Frontend/teach-share/src/components/text/EditText.vue
+++ Frontend/teach-share/src/components/text/EditText.vue
@@ -1,172 +1,155 @@
 <template>
 <body>
-
-
-    <!--We can use any buttons we want in the toolbar!
-    Check out the github for more examples, and the
-    exact code needed for other types of buttons -->
-    <h1>Text</h1>
-    <div>
-        <!-- These were in the editor but can't be commented out as part
-        of a tag so here they are. -->
-        <!-- @blur="onEditorBlur($event)"
-        @focus="onEditorFocus($event)" 
-        @ready="onEditorReady($event)" -->
+  <div class="card" style="padding: 10px;">
+    <div class="row">
+      <div class="col-12">
+        <!--We can use any buttons we want in the toolbar!
+        Check out the github for more examples, and the
+        exact code needed for other types of buttons -->
         <div class="custom-quill-editor">
-            <quill-editor v-model="element.content" :options="editorOption">
-                <div id="toolbar" slot="toolbar">
-                    <!-- Add a bold button -->
-                    <button class="ql-bold">Bold</button>
-                    <button class="ql-italic">Italic</button>
-                    <button class="ql-list">List</button>
-                    <!-- Add font size dropdown -->
-                    <select class="ql-size">
-                        <!-- Note a missing, thus falsy value, is used to reset to default -->
-                        <option selected></option>
-                        <option value="large"></option>
-                        <option value="huge"></option>
-                    </select>
-                    <select class="ql-font">
-                        <option selected="selected"></option>
-                        <option value="serif"></option>
-                        <option value="monospace"></option>
-                    </select>
-                    <!-- You can also add your own
-                    <button id="custom-button" @click="customButtonClick">[ Click me ]</button-->
-                </div>
-            </quill-editor>
-
-
-            <div class="quill-code">
+          <quill-editor
+            v-model="component.content"
+            :options="editorOption">
+            <div id="toolbar" slot="toolbar">
+              <!-- Add a bold button -->
+              <button class="ql-bold">Bold</button>
+              <button class="ql-italic">Italic</button>
+              <!-- Add font size dropdown -->
+              <select class="ql-size">
+                <!-- Note a missing, thus falsy value, is used to reset to default -->
+                <option selected></option>
+                <option value="large"></option>
+                <option value="huge"></option>
+              </select>
+              <select class="ql-font">
+                <option selected="selected"></option>
+                <option value="serif"></option>
+                <option value="monospace"></option>
+              </select>
+              <!-- You can also add your own
+              <button id="custom-button" @click="customButtonClick">[ Click me ]</button-->
             </div>
-            
+          </quill-editor>
+
+
+          <div class="quill-code">
+          </div>
         </div>
         
+      </div>
     </div>
     <div class="row">
+      <div class="offset-3 col-6">
+        <router-link :to="{name: 'create'}">
+        <button @click="submit" class="btn btn-primary btn-block">
+          Submit Text
+        </button>
+        </router-link>
+      </div>
+      <div class="col-2">
+        <router-link :to="{name:'create'}">
 
-        <div class="offset-3 col-6">
-            <router-link :to="{name: 'create'}">
-                <button @click="submit" class="btn btn-primary btn-block">
-                    Submit
-                </button>
-            </router-link>
-        </div>
-        <div class="col-2">
-            <router-link :to="{name: 'create'}">
-                <button type="button" class="btn btn-danger btn-block">
-                        Cancel
-                </button>
-            </router-link>
-        </div>
+        <button type="button" class="btn btn-danger btn-block" @click.prevent="close">
+        Cancel
+        </button>
+        </router-link>
+      </div>
     </div>
+  </div>
 </body>
 </template>
 
 <script>
 import Vue from "vue";
-import VueQuillEditor from "vue-quill-editor"
+import VueQuillEditor from "vue-quill-editor";
 
-import "quill/dist/quill.core.css"
-import "quill/dist/quill.snow.css"
-import "quill/dist/quill.bubble.css"
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
 import { mapState } from "vuex";
-Vue.use(VueQuillEditor,)
+Vue.use(VueQuillEditor);
 
 export default Vue.component("edit-text", {
-    data() {
-        return {
-            element: {},
-            editorOption: {
-                modules: {
-                    toolbar: "#toolbar"
-                }
-            } //sooooooooo many options to pass for customizing the editor.
+  data() {
+    return {
+      component: {},
+      editorOption: {
+        modules: {
+          toolbar: "#toolbar"
         }
-    },
-    computed: {
+      } //sooooooooo many options to pass for customizing the editor.
+    };
+  },
+  computed: {},
 
-
+  methods: {
+    submit: function(event) {
+      if (
+        this.$route.query.index ==
+        this.$store.state.create.postElements.length
+      ) {
+        this.$store.dispatch("addElement", this.component);
+      } else {
+        this.$store.dispatch("editElement", {
+          index: this.$route.query.index,
+          component: this.component
+        });
+      }
+      this.$router.push({ name: 'create'});
     },
-
-    methods: {
-        submit: function(event){
-            if(this.$route.query.index == this.$store.state.create.postElements.length){
-                this.$store.dispatch("addElement", this.element);
-            }
-            else{
-                this.$store.dispatch("editElement", {
-                    index : this.$route.query.index,
-                    element : this.element
-                });
-            }
-
-        },
-        close: function(event){
-            console.log("close");
-        },
-
-    },
-    mounted(){
-        this.$router.replace({name: "edit-text", query: {index: this.$route.query.index}})
-        console.log("mounted edit text");
-        
-		this.$store.dispatch("openEditor");
-        if(this.$route.query.index >= this.$store.state.create.postElements.length){
-            this.element = {
-                type: "text",
-                content: "",
-            }
-        }else{
-            this.element = Object.assign({}, this.$store.state.create.postElements[this.$route.query.index]);
-        }
-        
-    },
-    beforeDestroy(){
-        console.log("destroy");
-        this.$store.dispatch("closeEditor");
-    },
-    created() {
-        console.log("creating textedit");
-	},
-	destroyed() {
-		this.$store.dispatch("closeEditor");
-	}
-})
+    close: function(event) {
+      this.$router.push({name: 'create'});
+    }
+  },
+  mounted() {
+    this.$store.dispatch("openEditor");
+    this.$router.replace({
+      name: "edit-text",
+      query: { index: this.$route.query.index }
+    });
+    console.log("mounted edit text");
+    if (
+      this.$route.query.index >= this.$store.state.create.postElements.length
+    ) {
+      this.component = {
+        type: "text",
+        content: ""
+      };
+    } else {
+      this.component = Object.assign(
+        {},
+        this.$store.state.create.postElements[this.$route.query.index]
+      );
+    }
+  },
+  beforeDestroy() {
+    console.log("destroy");
+    this.$store.dispatch("closeEditor");
+  }
+});
 </script>
 
 <style lang="scss" scoped>
-
-.editor-card {
-    max-width: 100px;
+.quill-editor,
+.quill-code {
+  height: 75%;
+  width: 100%;
 }
 
 .custom-quill-editor {
-    // background: #F9F9F9;
-    width: 300px;
-    height: 100px;
+  background: #ffffff;
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 13em;
 }
-.quill-editor {
-    height: 100%;
+
+.quill-code {
+  border: none;
+  height: auto;
 }
 
-// .quill-code {
-//     max-width: 1000px;
-//     height: auto;
-//     border: 1px;
-// }
-
-// body {
-//     background-color: white;
-//     padding: 10px;
-// }
-
-// .quill-container{
-//     width: 1000px;
-//     /*background: #c0c0c0;*/
-//     font-family: "Roboto", sans-serif;
-//     -webkit-font-smoothing: antialiased;
-//     -moz-osx-font-smoothing: grayscale;
-//     border: 1;
-// }
+body {
+  
+}
 </style>
