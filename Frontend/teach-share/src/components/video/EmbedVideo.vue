d5226c7ca020b0c0e7fdf1b2eaf897ec0ec3cc48
bryan <bmccoid@gmail.com>
2018-03-01 11:29:59 -0800

    fixed dimension picker and looking at 'see more' faded text

--- Frontend/teach-share/src/components/video/EmbedVideo.vue
+++ Frontend/teach-share/src/components/video/EmbedVideo.vue
@@ -30,102 +30,100 @@
 import Vue from "vue";
 
 export default Vue.component("embed-video", {
-    props: [
-        "id",
-        "autoplay",
-        "width",
-        "height",
-        "controls",
-        "title",
-        "source",
-        "playlist",
-        "loop"
-    ],
-    data() {
-        return {
-            textClasses: [
-                'card-text',
-                'sizing-and-gradient'
-            ],
-            textShown: false,
-        };
+  props: [
+    "id",
+    "autoplay",
+    "width",
+    "height",
+    "controls",
+    "title",
+    "source",
+    "playlist",
+    "loop"
+  ],
+  data() {
+    return {
+      textClasses: ["card-text", "sizing-and-gradient"],
+      textShown: false
+    };
+  },
+  computed: {
+    cardWidth: function() {
+      return parseInt(this.width) + 20;
     },
-    computed: {
-        cardWidth: function() {
-            return parseInt(this.width) + 20;
-        },
-        aspectRatioClass: function() {
-            var ratio = this.width / this.height;
-            console.log(ratio);
-            // all the ratios are fudged down slightly to make the ratios match up more closely to
-            // values that are 'in-between'.
-            if (ratio >= 19 / 9) {
-                return "embed-responsive embed-responsive-21by9";
-            } else if (ratio >= 14 / 9) {
-                return "embed-responsive embed-responsive-16by9";
-            } else if (ratio >= 7 / 6) {
-                return "embed-responsive embed-responsive-4by3";
-            } else {
-                return "embed-responsive embed-responsive-1by1";
-            }
-        },
-        actualSource() {
-            var temp = this.source;
-            var url = new URL(this.source);
-            console.log(url);
-            var params = url.searchParams;
-
-            let videoID = params.get("v");
-            console.log(url.toString());
-
-            if (this.playlist) {
-                params.append("playlist", this.playlist);
-            }
-            if (this.autoplay) {
-                params.append("autoplay", "1");
-            }
-            if (this.loop) {
-                params.append("loop", "1");
-            }
-            if (this.controls) {
-                if (this.controls === true) {
-                    params.append("controls", "1");
-                } else {
-                    params.append("controls", "0");
-                }
-            }
-            return `https://youtube.com/embed/${videoID}`;
-        }
+    aspectRatioClass: function() {
+      var ratio = this.width / this.height;
+      console.log(ratio);
+      // all the ratios are fudged down slightly to make the ratios match up more closely to
+      // values that are 'in-between'.
+      if (ratio >= 19 / 9) {
+        return "embed-responsive embed-responsive-21by9";
+      } else if (ratio >= 14 / 9) {
+        return "embed-responsive embed-responsive-16by9";
+      } else if (ratio >= 7 / 6) {
+        return "embed-responsive embed-responsive-4by3";
+      } else {
+        return "embed-responsive embed-responsive-1by1";
+      }
     },
-    methods: {
-        showOrHideAllText() {
-            if (!this.textShown) {
-                this.textClasses.pop();
-            } else {
-                this.textClasses.push('sizing-and-gradient')
-            }
-            this.textShown = !this.textShown;
+    actualSource() {
+      var temp = this.source;
+      var url = new URL(this.source);
+      console.log(url);
+      var params = url.searchParams;
+
+      let videoID = params.get("v");
+      console.log(url.toString());
+
+      if (this.playlist) {
+        params.append("playlist", this.playlist);
+      }
+      if (this.autoplay) {
+        params.append("autoplay", "1");
+      }
+      if (this.loop) {
+        params.append("loop", "1");
+      }
+      if (this.controls) {
+        if (this.controls === true) {
+          params.append("controls", "1");
+        } else {
+          params.append("controls", "0");
         }
+      }
+      return `https://youtube.com/embed/${videoID}`;
     }
+  },
+  methods: {
+    showOrHideAllText() {
+      if (!this.textShown) {
+        this.textClasses.pop();
+      } else {
+        this.textClasses.push("sizing-and-gradient");
+      }
+      this.textShown = !this.textShown;
+    }
+  }
 });
 </script>
 
 <style lang="scss" scoped>
-
 .sizing-and-gradient {
-    max-height: 200px;
-    overflow:hidden;
-    background: linear-gradient(#333, #eee);
-    -webkit-background-clip: text;
-    color: transparent;
+  max-height: 200px;
+  overflow: hidden;
+  background: linear-gradient(#333, #eee);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
 }
 
-.fade-enter-active, .fade-leave-active {
-    transition: opacity 4s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 4s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
-
 </style>
 
