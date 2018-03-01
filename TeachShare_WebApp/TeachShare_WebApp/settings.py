b8f9ef4861abecf71502947c2735e677bee12d20
Bryan McCoid <bryan.mccoid@gmail.com>
2018-03-01 15:17:21 -0800

    added documents.py for the elasticsearch-dsl version of search.

--- TeachShare_WebApp/TeachShare_WebApp/settings.py
+++ TeachShare_WebApp/TeachShare_WebApp/settings.py
@@ -55,17 +55,15 @@ INSTALLED_APPS = [
     'social_django',
     'rest_framework_social_oauth2',
     'django_filters',
-    'haystack',
+    'django_elasticsearch_dsl',
 
     'accounts',
     'posts',
 ]
 
-HAYSTACK_CONNECTIONS = {
+ELASTICSEARCH_DSL={
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'teach',
+        'hosts': '127.0.0.1:9200'
     },
 }
 
