9b5ed55216948c6d236ce8b836b605e5dcd419b0
Bryan M <bryan.mccoid@gmail.com>
2018-04-14 18:30:16 -0700

    added settings to the deploy config.

--- TeachShare_WebApp/deploy.py
+++ TeachShare_WebApp/deploy.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 import sys
-import argcomplete, argparse
+import argcomplete
+import argparse
 from termcolor import colored, cprint
 import subprocess
 import yaml
 from shutil import copy
 
+
 class DockerCommandError(Exception):
     def __init__(self, cmd, message):
         self.cmd = cmd
         self.message = message
-        
+
 
 class Deploy(object):
 
     def __init__(self):
         cprint('Initializing deployment...', 'green')
-        
+
         self.load_config()
         for j in self.config['images']:
             print(j)
-        
-        self.images = [a + (':%s' % self.config['version']) for a in self.config['images']]
+
+        self.images = [a + (':%s' % self.config['version'])
+                       for a in self.config['images']]
         print(self.images)
 
-        parser = argparse.ArgumentParser(description='Build teach-share Django backend for Google Cloud production environment.', usage= \
-        '''deploy <command> [<args>]
+        parser = argparse.ArgumentParser(description='Build teach-share Django backend for Google Cloud production environment.', usage='''deploy <command> [<args>]
         
         The most commonly used commands are:
             build   Just build the backend assets
             deploy  Just deploy currently built backend assets
             all     Perform a complete deployment
-             ''' \
-            , add_help=False)
+             ''', add_help=False)
         parser.add_argument('command', help='build for production')
-        
+
         argcomplete.autocomplete(parser)
         self.args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, self.args.command):
@@ -57,27 +58,27 @@ class Deploy(object):
         print(args)
         print(args.settings)
         settings_resp = 'DEFAULT (settings.py)'
-        settings_path = ''
+        settings_path = self.config['settings']
         if str(args.settings) != 'None' and str(args.settings) != '':
             settings_path = args.settings
             settings_resp = args.settings
-        
+
         cprint('Running build command, settings=%s' % settings_resp, 'blue')
         print()
         print('\t--> Settings file: "%s"' % settings_path)
         print()
         # images = ['gcr.io/teach-share-200700/teachshare_web', 'gcr.io/teach-share-200700/teachshare_task']
         cprint('Building docker images...', 'blue', attrs=['bold'])
-        
+
         for i in self.images:
             build_result = self.build_docker_image(i, settings_path)
             if build_result.returncode != 0:
-                raise DockerCommandError(build_result.args, build_result.stderr)
+                raise DockerCommandError(
+                    build_result.args, build_result.stderr)
             else:
                 cprint('Finished building docker image: %s' % i, 'green')
-        cprint('Finished building all images!', color='green', attrs=['bold', 'reverse', 'blink'])
-
-        
+        cprint('Finished building all images!', color='green',
+               attrs=['bold', 'reverse', 'blink'])
 
     def push(self):
         parser = argparse.ArgumentParser(description='push backend images')
@@ -90,7 +91,7 @@ class Deploy(object):
         if str(args.settings) != 'None' and str(args.settings) != '':
             settings_path = args.settings
             settings_resp = args.settings
-        
+
         cprint('Running build command, settings=%s' % settings_resp, 'blue')
         print()
         print('\t--> Settings file: "%s"' % settings_path)
@@ -100,10 +101,12 @@ class Deploy(object):
         for i in self.images:
             build_result = self.push_docker_image(i)
             if build_result.returncode != 0:
-                raise DockerCommandError(build_result.args, build_result.stderr)
+                raise DockerCommandError(
+                    build_result.args, build_result.stderr)
             else:
                 cprint('Finished pushing docker image: %s' % i, 'green')
-        cprint('Finished pushing all images!', color='green', attrs=['bold', 'reverse', 'blink'])
+        cprint('Finished pushing all images!', color='green',
+               attrs=['bold', 'reverse', 'blink'])
 
     def load_config(self):
         with open('backend_deploy.yml', 'r') as ymlfile:
@@ -111,7 +114,12 @@ class Deploy(object):
         print(self.config)
 
     def build_docker_image(self, image_name, settings='TeachShare_WebApp.settings'):
-        result = subprocess.run(['docker', 'build', '--build-arg', 'settings_module=%s' % settings, '-t', image_name, '.'])
+        s = 'settings_module={}'.format(settings)
+        print('******************')
+        print(s)
+        print('******************')
+        result = subprocess.run(['docker', 'build', '--build-arg',
+                                 'settings_module=%s' % s, '-t', image_name, '.'])
         cprint(result, 'blue')
         return result
 
@@ -125,11 +133,10 @@ if __name__ == '__main__':
     try:
         Deploy()
     except DockerCommandError as err:
-        cprint('There was an error performing a docker command: %s, while running "%s"' % (err.message, " ".join(err.cmd)))
+        cprint('There was an error performing a docker command: %s, while running "%s"' % (
+            err.message, " ".join(err.cmd)))
 
     # result = subprocess.run(['ls', '-la'], stdout=subprocess.PIPE)
     # output_lines = str(result.stdout.decode('utf-8')).split('\n')
     # for l in output_lines:
     #     print(l)
-    
-    
