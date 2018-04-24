a1eb2a1fa62e611d77254d81581fd1459403ac0b
JL <larocque492@gmail.com>
2018-04-24 00:36:29 -0700

    Replaced old table with set rows/columns with new table that has its own component, and which the user can grow however they want by adding columns and entries dynamically. Good stuff!

--- Frontend/teach-share/src/components/table/EditTable.vue
+++ Frontend/teach-share/src/components/table/EditTable.vue
@@ -1,40 +1,15 @@
 <template>
     <div>
         <b-card>
-            <b-form>
-                <div class="modal-body row">
-                    <div class="colRowLabel col-md-3">
-                        <span><h5><strong>Number of rows:</strong></h5></span>
-                    </div>
-                    <div class="colRowInput">
-                        <b-form-input v-model.number="rowNum" type="number" placeholder="Enter the number of rows for the table" @change="tableResize()">
-                        </b-form-input>
-                    </div>
-                    <div class="colRowLabel col-md-3">
-                        <span><h5><strong>Number of columns: </strong></h5></span>
-                    </div>
-
-                    <div class="colRowInput">
-                        <b-form-input v-model.number="colNum" type="number" placeholder="Enter the number of columns for the table" @change="tableResize()">
-                        </b-form-input>
-                    </div>
-                </div>
-                    <div>
-                    <b-form-input v-model="colLabels" type="text" placeholder="Enter the names of the column labels" @change="tableCreate()">
-                    </b-form-input>
-                    </div>
-                <b-container>
-                    <b-row v-for="rows in rowNum">
-                        <b-col v-for="cols in colNum "> 
-                            <b-form-input class="tableEntry" v-model="cellIn[rows - 1] [cols - 1]" type="text" placeholder="Enter a data value or label" @change="tableCreate()">
-                            </b-form-input>
-                        </b-col>
-                    </b-row>
-                </b-container>
-            </b-form>
-
-            <b-table striped hover :items="table">
-            </b-table>
+            <h1>Create Data Table</h1>
+                <form v-on:submit.prevent="addColumn" class="form-inline my-2 my-lg-0">
+                    <span style="font-size: 1.5rem; padding-right: 1rem;">Add column:  </span>
+                    <input class="form-control mr-sm-1" type="text" v-model=columnToAdd aria-label="+">
+                    <button class="btn btn-dark my-2 my-sm-0" type="submit"><strong>+</strong></button>
+                </form>
+            <br>
+            <button v-if="this.columns.length > 0" @click="addItem" class="btn btn-primary">+ row entry</button> <br>
+            <VueBootstrapTable :columns=columns :values=values></VueBootstrapTable>
 
             <div class="row">
                 <div class="offset-3 col-6">
@@ -47,78 +22,143 @@
                     Cancel
                     </button>
                 </div>
-            </div>
+            </div> 
         </b-card>
+
 
     </div>
 </template>
 
 <script>
+
+var renderfu = function (colname, entry) {
+    return '<div class="btn-group" role="group" >'+
+        '  <button type="button" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button>'+
+        '  <button type="button" class="btn btn-sm btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>'+
+        '</div><span>'+JSON.stringify(entry)+'</span>';
+};
+
+var handleRow = function (event, entry) {
+    console.log("CLICK ROW: " + JSON.stringify(entry));
+};
     import Vue from "vue";
+    import BootstrapVue from "bootstrap-vue";
+    import VueBootstrapTable from "./Table";
+    Vue.use(VueBootstrapTable);
+    Vue.use(BootstrapVue);
 
 
     export default Vue.component("edit-table", {
+        components: {
+            VueBootstrapTable: VueBootstrapTable
+        },
+
         data() {
             return {
+            columnToAdd: "",
+            columns: [],
+            values: [],
+
+                inputValue: '',
+                currentRow: 0,
+                currentSlot: 0,
+                editing: false,
                 rowNum: 0,
                 colNum: 0,
                 colLabels: "",
                 cellIn: [],
                 table: [],
                 header: [],
-                showBarGraph: false,
-                showLineGraph: false,
-                showPieChart: false
+                fields: [
+                    'rowHeader',
+                    'sex',
+                    'age'
+                ],
+                items: [
+                    {rowHeader:'John', sex: 'Male', age: 32}, //items need keys
+                    {rowHeader:'Sally', sex: 'Female', age: 33}
+                ]
             }
         },
-
         methods: {
-            tableResize(){
-                this.cellIn = new Array(this.rowNum);
-                console.log(this.cellIn);
-                for(var i = 0; i < this.rowNum; i++){
-                    this.cellIn[i] = new Array(this.colNum);
-                    console.log(this.cellIn);
-                }
-                this.tableCreate();
+            test(event) {
+                console.log("Test value entered: ", event.target.value);
+                console.log(event);
+                // console.log("Value and index: ", value, index)
+                this.editing=false;
+                return "Hey it worked!";
             },
-
-            tableCreate(){
-                this.header = new Array(this.colNum);
-                this.header = this.colLabels.split(",");
-                console.log("column labels split: ", this.header);
-                this.table = new Array(this.rowNum);
-                for(var r = 0; r < this.rowNum; r++){
-                    var obj = {};
-                    for(var c = 0; c < this.colNum; c++){
-                        obj[this.header[c]] = this.cellIn [r][c];
-                    }
-                    this.table[r] = obj;
-                }
+            toggle() {
+                this.editing=true;
             },
-
             submit: function(event) {
                 if (
                     this.$route.query.index ==
                     this.$store.state.create.postElements.length
                 ) {
-                    this.$store.dispatch("addElement", {type: "table", content: this.table});
+                    this.$store.dispatch("addElement", {type: "table", content: this.values});
                 } else {
                     this.$store.dispatch("editElement", {
                         index: this.$route.query.index,
-                        element:{type: "table", content: this.table}
+                        element:{type: "table", content: this.values}
                     });
                 }
                 this.$router.push({ name: "create" });
             },
             close: function(event) {
                 this.$router.push({ name: "create" });
+            },
+        addItem: function () {
+            var self = this;
+            var item = {
+                // "id": this.values.length + 1,
+                // "name": "name " + (this.values.length + 1)
+            };
+            for(var r = 0; r < this.columns.length; r++){
+                let key = this.columns[r].title;
+                console.log("Key to add: ", key);
+                item[key] = "__";
+            };
+            console.log("Adding item: ", item);
+            this.values.push(item);
+        },
+        addColumn: function () {
+            if (this.columnToAdd != "") {
+                var self = this
+                var item = {
+                    title: this.columnToAdd,
+                    visible: true,
+                    editable: true
+                }
+                for (var i = 0; i < this.values.length; i++){
+                    this.values[i][this.columnToAdd] = "__";
+                    console.log("Push column: ", this.columnToAdd, "onto item:  ", this.values[i]);
+                }
+                this.columns.push(item);
             }
+        },
+        toggleFilter: function () {
+            this.showFilter = !this.showFilter;
+        },
+        togglePicker: function () {
+            this.showPicker = !this.showPicker;
+        },
+        togglePagination: function () {
+            this.paginated = !this.paginated;
         }
+    }
+
+      
     });
 </script>
 
 <style lang="scss" scoped>
+    .rowLabelInput {
+        width: 8rem
+    }
+    .tableEntryInput {
+        width: 4rem
+    }
     .colRowLabel {
         padding-top: 5px;
     }
---
+++ Frontend/teach-share/src/components/table/Table.vue
@@ -0,0 +1,501 @@
+<template>
+            <table class="table table-bordered table-hover table-condensed table-striped vue-table">
+                <thead>
+                    <tr>
+                        <th v-for="column in displayCols" @click="sortBy($event, column.name)"
+                            :key="column">
+                            <!-- :class="getClasses(column)"-->
+                            {{ column.title }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="entry in values" :key="entry" @click="rowClickHandler($event, entry)">
+                        <td v-for="column in displayColsVisible" :key="column"
+                            v-show="column.visible" :class="column.cellstyle">
+                            <span v-if="column.renderfunction!==false" v-html="column.renderfunction( column.name, entry )"></span>
+                            <span v-else-if="!column.editable"> {{ entry[column.name] }} </span>
+                            <value-field-section v-else
+                                :entry="entry"
+                                :columnname="column.name"></value-field-section>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+ 
+</template>
+
+<style>
+
+    .vue-table {
+
+    }
+
+    /*#maindiv {
+        content: " ";
+        box-sizing: border-box;
+        display:
+        table; width: 100%;
+    }
+*/
+    .vue-table-loading .spinner {
+        border: 16px solid #f3f3f3; /* Light grey */
+        border-top: 16px solid #3498db; /* Blue */
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin: -60px 0 0 -60px;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
+
+    .vue-table-loading{
+        position: absolute;
+        z-index: 99;
+        background-color: #ddd;
+        opacity: 0.5;
+        width: 100%;
+        height: 100%;
+    }
+
+    .vue-table-loading-hidden {
+        display: none;
+    }
+
+    table.vue-table thead > tr > th {
+        cursor: pointer;
+        padding-right: 30px !important;
+    }
+
+
+    .vue-table .editableField {
+        cursor:pointer;
+    }
+
+    .vue-table .selected-cell {
+        background-color: #F7C072;
+    }
+
+    .vue-table .selected-row {
+        background-color: #FAE1BE /*!important;*/
+    }
+</style>
+
+<script>
+    import Vue from "vue";
+    import FontAwesomeIcon from "@fortawesome/vue-fontawesome";
+    import axios from 'axios';
+    import qs from 'qs';
+    import lodashorderby from 'lodash.orderby';
+    import lodashincludes from 'lodash.includes';
+    import lodashfindindex from 'lodash.findindex';
+
+    /* Field Section used for displaying and editing value of cell */
+    var valueFieldSection = {
+      template: '<span v-if="!enabled" @click="toggleInput" class="editableField">{{this.entry[this.columnname]}}</span>'+
+          '<div v-else-if="enabled" class="input-group">'+
+          '  <input type="text" class="form-control" id="number" v-model="datavalue" @keyup.enter="saveThis" @keyup.esc="cancelThis">'+
+          '  <span class="input-group-btn">'+
+          '    <button class="btn btn-danger" type="button" @click="cancelThis" ><span>X</span></button>'+
+          '    <button class="btn btn-primary" type="button" @click="saveThis" ><font-awesome-icon icon="check" fixed-width></font-awesome-icon></button>'+
+          '  </span>'+
+          '</div>',
+    components: { FontAwesomeIcon},
+      props: ['entry','columnname'],
+      data () {
+          return {
+            enabled: false,
+            datavalue: "__",
+          }
+      },
+      methods: {
+        saveThis: function () {
+            var originalValue = this.entry[this.columnname];
+            this.entry[this.columnname] = this.datavalue;
+            this.$parent.$emit('cellDataModifiedEvent', originalValue, this.datavalue, this.columnname,  this.entry);
+            this.enabled = !this.enabled;
+        },
+        cancelThis: function () {
+            this.datavalue = this.entry[this.columnname];
+            this.enabled = !this.enabled;
+        },
+        toggleInput: function () {
+            this.datavalue= this.entry[this.columnname];
+            this.enabled=!this.enabled;
+        },
+      }
+    };
+
+    export default {
+        name: "VueBootstrapTable",
+        components: {
+            'value-field-section': valueFieldSection,
+            FontAwesomeIcon: FontAwesomeIcon
+        },
+        props: {
+            /**
+             * The column titles, required
+             */
+            columns: {
+                type: Array,
+                required: true,
+            },
+            /**
+             * The rows, an Array of objects
+             */
+            values: {
+                type: Array,
+                required: false,
+            },
+            /**
+             * Enable/disable table sorting, optional, default true
+             */
+            sortable: {
+                type: Boolean,
+                required: false,
+                default: true,
+            },
+            /**
+             * Enable/disable table multicolumn sorting, optional, default false.
+             * Also sortable must be enabled for this function to work.
+             */
+            multiColumnSortable: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            /**
+             * Enable/disable input filter, optional, default false
+             */
+            showFilter: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            /**
+             * Define if Filter search field is to work in a case Sensitive way. Default: true
+             */
+            filterCaseSensitive: {
+                type: Boolean,
+                required: false,
+                default: true,
+            },
+            /**
+             * Enable/disable column picker to show/hide table columns, optional, default false
+             */
+            showColumnPicker: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            /**
+             * Enable/disable pagination for the table, optional, default false
+             */
+            paginated: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            /**
+             * If pagination is enabled defining the page size, optional, default 10
+             */
+            pageSize: {
+                type: Number,
+                required: false,
+                default: 10,
+            },
+            /**
+             * If loading of table is to be done through ajax, then this object must be set
+             */
+            ajax: {
+                type: Object,
+                required: false,
+                default: function () {
+                    return {
+                        enabled: false,
+                        url: "",
+                        method: "GET",
+                        delegate: false,
+                        axiosConfig: {}
+                    }
+                }
+            },
+            /**
+             * Function to handle row clicks
+             */
+            rowClickHandler: {
+                type: Function,
+                required: false,
+                default: function () {}
+            },
+        },
+        data () {
+            return {
+                filteredSize: 0,
+                filterKey: "",
+                sortKey: [],
+                sortOrders: {},
+                sortChanged: 1,
+                columnMenuOpen: false,
+                displayCols: [],
+                filteredValues: [],
+                page: 1,
+                echo: 0,
+                loading: false,
+            };
+        },
+        mounted: function () {
+          this.$nextTick(function () {
+              this.loading= true;
+            //   this.setSortOrders();
+              var self = this;
+              this.columns.forEach(function (column) {
+                  var obj = self.buildColumnObject(column);
+                  self.displayCols.push(obj);
+              });
+              this.processFilter();
+          })
+        },
+        created: function () {
+            var self = this ;
+            this.$on('cellDataModifiedEvent', self.fireCellDataModifiedEvent);
+        },
+        beforeDestroy: function(){
+            var self = this ;
+            this.$off('cellDataModifiedEvent', self.fireCellDataModifiedEvent);
+        },
+        watch: {
+            values: function () {
+                this.processFilter();
+            },
+            columns: function () {
+                this.displayCols = [];
+                var self = this;
+                this.columns.forEach(function (column) {
+                    var obj = self.buildColumnObject(column);
+                    self.displayCols.push(obj);
+                });
+                this.setSortOrders();
+                this.processFilter();
+            },
+        //     showFilter: function () {
+        //         this.filterKey = "";
+        //     },
+        //     showColumnPicker: function () {
+        //         this.columnMenuOpen = false;
+
+        //         this.displayCols.forEach(function (column) {
+        //             column.visible = true;
+        //         });
+        //     },
+            filterKey: function () {
+                // filter was updated, so resetting to page 1
+                this.page = 1;
+                this.processFilter();
+            },
+            sortKey: function () {
+                this.processFilter();
+            },
+            sortChanged: function () {
+                this.processFilter();
+            },
+            page: function () {
+                this.processFilter();
+            },
+            paginated: function () {
+                this.processFilter();
+            },
+            loading: function () {
+                /*document.getElementById("loadingdiv").style.width = document.getElementById("maindiv").getBoundingClientRect().width + "px";
+                document.getElementById("loadingdiv").style.height = document.getElementById("maindiv").getBoundingClientRect().height+"px";*/
+            }
+        },
+        computed: {
+            displayColsVisible: function () {
+                var displayColsVisible = [];
+                for (var a in this.displayCols) {
+                    if (this.displayCols[a].visible)
+                        displayColsVisible.push(this.displayCols[a]);
+                }
+                return displayColsVisible;
+            },
+            filteredValuesSorted: function () {
+                var tColsDir = [];
+                for(var i=0, len=this.sortKey.length; i < len; i++){
+                    tColsDir.push(this.sortOrders[this.sortKey[i]].toLowerCase());
+                }
+                return lodashorderby(this.filteredValues, this.sortKey , tColsDir);
+            },
+            validPageNumbers: function () {
+                // 5 page max
+                var result = [];
+                var start = 1;
+                if (this.page > 3)
+                    start = this.page-2;
+                for ( var i = 0 ; start <= this.maxPage && i<5; start++ ) {
+                    result.push(start);
+                    i++;
+                }
+                return result;
+            },
+            maxPage: function () {
+                return Math.ceil(this.filteredSize / this.pageSize);
+            },
+            showPaginationEtc: function () {
+                var temp = 1;
+                if (this.page > 3)
+                    temp = this.page-2;
+                return ( (temp+4) < this.maxPage  );
+            },
+        },
+        methods: {
+            fireCellDataModifiedEvent:function ( originalValue, newValue, columnTitle, entry) {
+                this.$parent.$emit('cellDataModifiedEvent',originalValue, newValue, columnTitle, entry);
+            },
+            processFilter: function () {
+            var self = this;
+            var result = this.values.filter(item => {
+                                var good = false;
+                                for (var col in self.displayColsVisible) {
+                                    if (self.filterCaseSensitive) {
+                                        if (lodashincludes(item[self.displayColsVisible[col].name] + "", self.filterKey + "")) {
+                                            good = true;
+                                        }
+                                    } else {
+                                        if (lodashincludes((item[self.displayColsVisible[col].name] + "").toLowerCase(), (self.filterKey + "").toLowerCase())) {
+                                            good = true;
+                                        }
+                                    }
+
+                                }
+                                return good;
+                    });
+
+                    var tColsDir = [];
+                    for(var i=0, len=this.sortKey.length; i < len; i++){
+                        tColsDir.push(this.sortOrders[this.sortKey[i]].toLowerCase());
+                    }
+
+                    result = lodashorderby(result, this.sortKey, tColsDir);
+
+                    this.filteredSize = result.length;
+                    if (this.paginated) {
+                        var startIndex = (this.page - 1) * this.pageSize;
+                        var tIndex = 0;
+                        var tempResult = [];
+                        while (tIndex < this.pageSize) {
+                            if (typeof result[startIndex + tIndex] !== "undefined")
+                                tempResult.push(result[startIndex + tIndex]);
+                            tIndex++;
+                        }
+                        self.filteredValues = tempResult;
+                    } else
+                        self.filteredValues = result;
+                    self.loading = false;
+                // }
+            },
+            buildColumnObject: function (column) {
+                var obj = {};
+                obj.title = column.title;
+                if ( typeof column.name !== "undefined")
+                    obj.name = column.name;
+                else
+                    obj.name = column.title;
+                if ( typeof column.visible !== "undefined")
+                    obj.visible = column.visible;
+                else
+                    obj.visible = true;
+                if ( typeof column.editable !== "undefined")
+                    obj.editable = column.editable;
+                else
+                    obj.editable = false;
+                if ( typeof column.renderfunction !== "undefined")
+                    obj.renderfunction = column.renderfunction;
+                else
+                    obj.renderfunction = false;
+                if ( typeof column.columnstyle !== "undefined")
+                    obj.columnstyle = column.columnstyle;
+                else
+                    obj.columnstyle = "";
+                if ( typeof column.cellstyle !== "undefined")
+                    obj.cellstyle = column.cellstyle;
+                else
+                    obj.cellstyle = "";
+
+                return obj;
+            },
+            setSortOrders: function () {
+                this.sortKey = [];
+                var sortOrders = {};
+                this.columns.forEach(function (column) {
+                    sortOrders[column.name] = "";
+                });
+                this.sortOrders = sortOrders;
+
+            },
+            sortBy: function (event, key) {
+                if (this.sortable) {
+                    var self = this;
+
+                    if (!this.multiColumnSortable || ( this.multiColumnSortable && !event.shiftKey)) {
+                        this.sortKey = [key];
+                        this.columns.forEach(function (column) {
+                            if (column.name !== key) {
+                                self.sortOrders[column.name] = "";
+                            }
+                        });
+                    } else {
+                        if (lodashfindindex(this.sortKey, function(o) { return o === key; }) === -1) {
+                            this.sortKey.push(key);
+                        }
+                    }
+                    if (this.sortOrders[key] === "") {
+                        this.sortOrders[key] = "ASC";
+                    } else if (this.sortOrders[key] === "ASC") {
+                        this.sortOrders[key] = "DESC";
+                    } else {
+                        this.sortOrders[key] = "ASC";
+                    }
+
+                    this.sortChanged = this.sortChanged * -1;
+                }
+            },
+            // getClasses: function (column) {
+            //     var classes = [column.columnstyle];
+            //     var key = column.name;
+            //     if (this.sortable) {
+            //         classes.push("arrow");
+            //         /*if (this.sortKey === key) {
+            //             classes.push("active");
+            //         }*/
+            //         if (lodashfindindex(this.sortKey, function(o) { return o === key; }) !== -1) {
+            //             classes.push("active");
+            //         }
+
+            //         if (this.sortOrders[key] === "ASC") {
+            //             classes.push("asc");
+            //         } else if (this.sortOrders[key] === "DESC") {
+            //             classes.push("dsc");
+            //         }
+            //     }
+            //     return classes;
+            // },
+            toggleColumn: function (column) {
+                column.visible = !column.visible;
+            },
+            closeDropdown: function () {
+                this.columnMenuOpen = false;
+            },
+        },
+        events: {
+        }
+    }
+</script>
